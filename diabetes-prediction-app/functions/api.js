38124908b1ba06b1e9c3e604783b4f6c4e266b84
raghzzzspace <162113357+raghzzzspace@users.noreply.github.com>
2024-10-08 22:47:44 +0530

    .

--- diabetes-prediction-app/functions/api.js
+++
@@ -1,8 +0,0 @@
-// functions/api.js
-exports.handler = async (event, context) => {
-    const data = { message: "Hello from your backend!" };
-    return {
-        statusCode: 200,
-        body: JSON.stringify(data),
-    };
-};
